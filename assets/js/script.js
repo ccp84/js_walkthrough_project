15d4a57f70a79120f76d38dda8dd636aa76afe14
ccp84 <ccp84@icloud.com>
2022-09-04 15:14:24 +0000

    JS>Calculate answer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,8 +40,21 @@ function checkAnswer() {
 
 }
 
+/**
+ * Get arguments for question from the DOM and return the correct answer.
+ */
 function calculateCorrectAnswer() {
 
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').innerText;
+
+    if (operator === '+') {
+        return [operand1 + operand2, 'addition'];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}`;
+    }
 }
 
 function incrementScore() {
@@ -57,7 +70,7 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = '+';
-    
+
 }
 
 function displaySubtractQuestion() {
