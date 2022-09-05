91aec3d8c4b7fecee566001a148e79a78d4f1c7f
ccp84 <ccp84@icloud.com>
2022-09-05 12:32:05 +0000

    JS>Clear answer box and focus

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,6 +24,9 @@ document.addEventListener("DOMContentLoaded", function() {
  */
 function runGame(gameType) {
 
+    document.getElementById('answer-box').value = "";
+    document.getElementById('answer-box').focus();
+
     //Generate random numbers
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
